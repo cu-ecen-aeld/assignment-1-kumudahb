267a15c686e40e5697303f553d3b9e78466e894b
kumudahb <kumudahb@nie.ac.in>
2026-03-20 09:51:29 +0530

    Test_validate_username.c

--- student-test/Test_validate_username.c
+++ student-test/Test_validate_username.c
@@ -1,15 +1,24 @@
 #include "unity.h"
-#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "../../examples/autotest-validate/autotest-validate.h"
+#include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
-/* Function provided by the assignment code */
-extern int validate_username(const char *username);
-
-/* Test function */
-void test_validate_username(void)
+/**
+* This function should:
+*   1) Call the my_username() function in Test_assignment_validate.c to get your hard coded username.
+*   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
+*       the assignment autotest submodule at assignment-autotest/test/assignment1/
+*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE the two strings are equal.  See
+*       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
+*/
+void test_validate_my_username()
 {
-    /* This should match your github username */
-    TEST_ASSERT_EQUAL(1, validate_username("kumudahb"));
+    /**
+     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
+     * config file and my_username() functions are setup properly
+     */
 
-    /* Test with an incorrect username */
-    TEST_ASSERT_EQUAL(0, validate_username("wrongusername"));
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    TEST_ASSERT_TRUE_MESSAGE(my_username(),"AESD students, please fix me!");
 }
